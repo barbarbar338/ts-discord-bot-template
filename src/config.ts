e170a2d0baca8b6891110b393c290ff598d4819a
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-03-13 19:08:06 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ config();
 export const CONFIG = {
 	IS_DEV: process.env.NODE_ENV === "development",
 	TOKEN: process.env.TOKEN as string,
-	OWNERS: ["331846231514939392"],
+	OWNERS: ["952574663916154960"],
 	PREFIX: "!",
 	COOLDOWN: 5000,
 };
